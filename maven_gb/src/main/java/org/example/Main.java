33f01a470e1c020d17316efb0895a42d5d13009c
skachkovGB <skachok2la@gmail.com>
2023-05-25 16:46:01 +0300

    true maven

--- maven_gb/src/main/java/org/example/Main.java
+++ maven_gb/src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
+
+
         System.out.println("Hello world!");
     }
 }
